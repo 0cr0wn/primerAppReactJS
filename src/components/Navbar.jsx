3305d5e0e46e3fc572aa75ad2a41bc0a6a34ed83
Ocrown <tuliocrown@gmail.com>
2022-07-19 20:33:56 -0400

    navBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = ({titulo,subtitulo}) => {
+
+  return (
+    <div  >
+    
+    <div>{titulo}</div>
+    <div>{subtitulo}</div>
+    </div>
+  )
+}
+
+export default Navbar
+
